a40ac15f945dc2b4af231f25e5ce6a6acfceebaf
bert <btemme@hotmail.com>
2018-08-29 10:50:11 +0200

    add error handling

--- src/pointer/pointer/MainPage.xaml.cs
+++ src/pointer/pointer/MainPage.xaml.cs
@@ -15,6 +15,7 @@ namespace pointer
         private double headingNorth;
         private double longitude;
         private double latitude;
+        private double minimumDistance = 10;
 
         public MainPage()
         {
@@ -44,7 +45,6 @@ namespace pointer
         {
             longitude = e.Position.Longitude;
             latitude = e.Position.Latitude;
-            DoWork();
         }
 
         void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
@@ -53,80 +53,114 @@ namespace pointer
             headingNorth = data.HeadingMagneticNorth;
         }
 
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            DoWork();
+        }
+
         private void DoWork()
         {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                tijd.Text = "tijd: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                richting.Text = "richting: " + Math.Round(headingNorth, 0) + "°";
+            });
+
             if (longitude > 0 && latitude > 0)
             {
-                var delta = 0.001;
-                var env = $"{longitude - delta},{latitude - delta},{longitude + delta},{latitude + delta}";
-                var features = Ngr.GetPanden(env);
-
-                var loc1 = new NetTopologySuite.Geometries.Point(longitude, latitude);
-
-                // create a line
-                var c2 = new Coordinate(longitude + Math.Sin(headingNorth) * 0.01, latitude + Math.Sin(headingNorth) * 0.01);
-                var c1 = new Coordinate(longitude, latitude);
-
-                var line = new NetTopologySuite.Geometries.LineString(new Coordinate[] { c1, c2 });
-
-                Feature nearestPand=null;
-                double distanceNearest = Double.MaxValue;
-
-                foreach (var pand in features)
-                {
-                    var geom = pand.Geometry;
-                    var geomtype = geom.Type;
-                    var geomstring = JsonConvert.SerializeObject(geom);
-                    var reader = new GeoJsonReader();
-                    // todo: check for multipolygon versus polygon
-
-                    IGeometry pandGeom;
-                    if (geomtype == GeoJSON.Net.GeoJSONObjectType.MultiPolygon)
-                    {
-                        pandGeom = reader.Read<NetTopologySuite.Geometries.MultiPolygon>(geomstring);
-                    }
-                    else
-                    {
-                        pandGeom = reader.Read<NetTopologySuite.Geometries.Polygon>(geomstring);
-                    }
-
-                    var intersects = pandGeom.Intersects(line);
-
-                    if (intersects)
-                    {
-                        var dist = loc1.Distance(pandGeom);
-
-                        if (dist < distanceNearest)
-                        {
-                            distanceNearest = dist;
-                            nearestPand = pand;
-                        }
-                    }
-                }
-
                 Device.BeginInvokeOnMainThread(() =>
                 {
-                    afstand.Text = "afstand: " + distanceNearest;
-
-                    if (nearestPand != null)
-                    {
-                        identificatie.Text = "identificatie: " + nearestPand.Properties["identificatie"];
-                        bouwjaar.Text = "bouwjaar: " + nearestPand.Properties["bouwjaar"];
-                        status.Text = "status: " + nearestPand.Properties["status"];
-                        gebruiksdoel.Text = "gebruiksdoel: " + nearestPand.Properties["gebruiksdoel"];
-                        oppervlakte.Text = "oppervlakte: " + nearestPand.Properties["oppervlakte_max"];
-                        verblijfsobjecten.Text = "verblijfsobjecten: " + nearestPand.Properties["aantal_verblijfsobjecten"];
-                    }
-                    else
-                    {
-                        identificatie.Text = "-";
-                        bouwjaar.Text = "-";
-                        status.Text = "-";
-                        gebruiksdoel.Text = "-";
-                        oppervlakte.Text = "-";
-                        verblijfsobjecten.Text = "-";
-                    }
+                    gps.Text = "gps: " + Math.Round(longitude,4) + ", " + Math.Round(latitude,4);
                 });
+
+                var delta = 0.001;
+                var env = $"{longitude - delta},{latitude - delta},{longitude + delta},{latitude + delta}";
+
+                var current = Connectivity.NetworkAccess;
+
+                if (current == NetworkAccess.Internet)
+                {
+                    var features = Ngr.GetPanden(env);
+
+                    var loc1 = new NetTopologySuite.Geometries.Point(longitude, latitude);
+
+                    // create a line
+                    var c2 = new Coordinate(longitude + Math.Sin(headingNorth) * 0.01, latitude + Math.Cos(headingNorth) * 0.01);
+                    var c1 = new Coordinate(longitude, latitude);
+
+                    var line = new NetTopologySuite.Geometries.LineString(new Coordinate[] { c1, c2 });
+
+                    Feature nearestPand = null;
+                    double distanceNearest = Double.MaxValue;
+
+                    foreach (var pand in features)
+                    {
+                        var geom = pand.Geometry;
+                        var geomtype = geom.Type;
+                        var geomstring = JsonConvert.SerializeObject(geom);
+                        var reader = new GeoJsonReader();
+                        // todo: check for multipolygon versus polygon
+
+                        IGeometry pandGeom;
+                        if (geomtype == GeoJSON.Net.GeoJSONObjectType.MultiPolygon)
+                        {
+                            pandGeom = reader.Read<NetTopologySuite.Geometries.MultiPolygon>(geomstring);
+                        }
+                        else
+                        {
+                            pandGeom = reader.Read<NetTopologySuite.Geometries.Polygon>(geomstring);
+                        }
+
+                        var intersects = pandGeom.Intersects(line);
+
+                        if (intersects)
+                        {
+                            var dist = loc1.Distance(pandGeom);
+
+                            if (dist < distanceNearest && dist > minimumDistance / 111000)
+                            {
+                                distanceNearest = dist;
+                                nearestPand = pand;
+                            }
+                        }
+                    }
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        afstand.Text = "afstand: " + Math.Round(distanceNearest * 111000, 0) + "m";
+
+                        if (nearestPand != null)
+                        {
+                            identificatie.Text = "identificatie: " + nearestPand.Properties["identificatie"];
+                            bouwjaar.Text = "bouwjaar: " + nearestPand.Properties["bouwjaar"];
+                            status.Text = "status: " + nearestPand.Properties["status"];
+                            gebruiksdoel.Text = "gebruiksdoel: " + nearestPand.Properties["gebruiksdoel"];
+                            oppervlakte.Text = "oppervlakte: " + nearestPand.Properties["oppervlakte_max"];
+                            verblijfsobjecten.Text = "verblijfsobjecten: " + nearestPand.Properties["aantal_verblijfsobjecten"];
+                        }
+                        else
+                        {
+                            afstand.Text = "helaas, geen pand gevonden";
+                            identificatie.Text = "";
+                            bouwjaar.Text = "";
+                            status.Text = "";
+                            gebruiksdoel.Text = "";
+                            oppervlakte.Text = "";
+                            verblijfsobjecten.Text = "";
+                        }
+                    });
+                }
+                else
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        afstand.Text = "geen internet connectie :-(";
+                    });
+                }
+            }
+            else
+            {
+                afstand.Text = "gps niet beschikbaar :-(";
             }
         }
     }
--- src/pointer/pointer/Ngr.cs
+++ src/pointer/pointer/Ngr.cs
@@ -1,5 +1,6 @@
 ﻿using GeoJSON.Net.Feature;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 
@@ -9,15 +10,23 @@ namespace pointer
     {
         public static List<Feature> GetPanden(string envelope)
         {
+            var result = new List<Feature>();
             var client = new HttpClient();
 
             var url = $"http://geodata.nationaalgeoregister.nl/bag/wfs?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&TYPENAME=bag:pand&SRSNAME=EPSG:4326&cql_filter=(bbox(geometrie,{envelope},%27EPSG:4326%27))&outputformat=application/json";
 
-            var response = client.GetAsync(url).Result;
-            var responseString = response.Content.ReadAsStringAsync().Result;
+            try
+            {
+                var response = client.GetAsync(url).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
 
-            var featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(responseString);
-            return featureCollection.Features;
+                var featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(responseString);
+                return featureCollection.Features;
+            }
+            catch(Exception){
+                // continue, there must be internet error
+            }
+            return result;
 
         }
 
